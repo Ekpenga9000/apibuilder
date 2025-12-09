54ba8f8b646c8e0b44f69026506ecc4765bf2d5d
Louis <ekpenga900@gmail.com>
2025-12-08 18:18:40 -0800

    Create placeholder for Dashboard ui

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -4,13 +4,18 @@ import {
   GoXCircleFill,
   GoServer,
   GoPlusCircle,
+  GoArrowSwitch,
+  GoHistory,
+  GoMail,
+  GoAlert,
 } from "react-icons/go";
+import { FiCheckCircle, FiInfo } from "react-icons/fi";
 
 const Dashboard = () => {
   return (
     <section className="py-4 px-6 flex-1">
       <h1 className="font-bold text-4xl mb-4">Welcome, Alice!</h1>
-      <ul className="mb-4 flex items-center gap-4">
+      <ul className="mb-8 flex items-center gap-4">
         <li className="flex items-center">
           <span className="font-semibold">5</span>
           <span className="ml-2 flex items-center gap-1 text-gray-600">
@@ -40,7 +45,7 @@ const Dashboard = () => {
           </span>
         </li>
       </ul>
-      <div className="border border-gray-300 rounded-md p-4">
+      <div className="border border-gray-300 rounded-md p-4 mb-6">
         <ul className="flex items-center justify-between mb-4">
           <li className="font-semibold text-lg">My Servers</li>
           <li className="flex items-center gap-3 text-sm font-semibold cursor-pointer">
@@ -70,7 +75,7 @@ const Dashboard = () => {
                 <td className="py-3 px-4">1</td>
                 <td className="py-3 px-4 text-gray-600 text-sm">1 day ago</td>
                 <td className="py-3 px-4">
-                  <button className="text-blue-600 hover:underline">
+                  <button className="text-blue-600 hover:underline text-sm">
                     Edit
                   </button>
                 </td>
@@ -82,7 +87,7 @@ const Dashboard = () => {
                 <td className="py-3 px-4">1</td>
                 <td className="py-3 px-4 text-gray-600 text-sm">2 days ago</td>
                 <td className="py-3 px-4">
-                  <button className="text-blue-600 hover:underline">
+                  <button className="text-blue-600 hover:underline text-sm">
                     Edit
                   </button>
                 </td>
@@ -96,7 +101,7 @@ const Dashboard = () => {
                 <td className="py-3 px-4">0</td>
                 <td className="py-3 px-4 text-gray-600 text-sm">3 days ago</td>
                 <td className="py-3 px-4">
-                  <button className="text-blue-600 hover:underline">
+                  <button className="text-blue-600 hover:underline text-sm">
                     Edit
                   </button>
                 </td>
@@ -110,7 +115,7 @@ const Dashboard = () => {
                 <td className="py-3 px-4">0</td>
                 <td className="py-3 px-4 text-gray-600 text-sm">3 weeks ago</td>
                 <td className="py-3 px-4">
-                  <button className="text-blue-600 hover:underline">
+                  <button className="text-blue-600 hover:underline text-sm">
                     Edit
                   </button>
                 </td>
@@ -119,6 +124,156 @@ const Dashboard = () => {
           </table>
         </div>
       </div>
+      <section>
+        <ul className="grid grid-cols-3 gap-6">
+          <li className="border border-gray-300 rounded-md p-4">
+            <div className="flex items-center justify-between">
+              <h4 className="font-semibold text-lg mb-4 flex items-center gap-2">
+                {" "}
+                <GoHistory />
+                Recent Activity
+              </h4>
+
+              <button className="text-blue-600 hover:underline text-sm cursor-pointer inline-block mb-4">
+                view all
+              </button>
+            </div>
+            <ul>
+              <li className="mb-4 flex items-start gap-3">
+                <div>
+                  <FiCheckCircle className="text-green-600 inline-block mr-2" />
+                </div>
+                <div>
+                  <p className="text-sm">
+                    API 'User Login' created in Product Catalog API project.{" "}
+                  </p>
+                  <span className="text-gray-500 text-xs">2 hours ago</span>
+                </div>
+              </li>
+              <li className="mb-4 flex items-start gap-3">
+                <div>
+                  <FiInfo className="text-blue-600 inline-block mr-2" />
+                </div>
+                <div>
+                  <p className="text-sm">
+                    Server 'Order Processing' status changed to inactive.
+                  </p>
+                  <span className="text-gray-500 text-xs">5 hours ago</span>
+                </div>
+              </li>
+              <li className="mb-4 flex items-start gap-3">
+                <div>
+                  <GoAlert className="text-red-600 inline-block mr-2" />
+                </div>
+                <div>
+                  <p className="text-sm">
+                    Failed to connect to Database server for Inventory
+                    Management.
+                  </p>
+                  <span className="text-gray-500 text-xs">1 day ago</span>
+                </div>
+              </li>
+              <li className="mb-4 flex items-start gap-3">
+                <div>
+                  <GoAlert className="text-red-600 inline-block mr-2" />
+                </div>
+                <div>
+                  <p className="text-sm">
+                    High latency detected on Payment Gateway Integration API.
+                  </p>
+                  <span className="text-gray-500 text-xs">1 day ago</span>
+                </div>
+              </li>
+            </ul>
+            {/* <div className="h-40  flex items-center justify-center text-gray-600">
+              No recent activity to display.
+            </div> */}
+            {/* <div>
+              <button className="mt-4 text-blue-600 hover:underline">
+                View All Activity
+              </button>
+            </div> */}
+          </li>
+          <li className="border border-gray-300 rounded-md p-4">
+            <div>
+              <h4 className="font-semibold text-gray-600 mb-4 flex items-center justify-between">
+                Recent Requests (24 hrs)
+                <GoArrowSwitch />
+              </h4>
+              <ul>
+                <li className="mb-4 flex items-start justify-between">
+                  <div>
+                    <p className="text-sm">User Service Management</p>
+                    <span className="text-gray-500 text-xs">
+                      1,250 requests
+                    </span>
+                  </div>
+                  <span className="font-semibold">1,250</span>
+                </li>
+                <li className="mb-4 flex items-start justify-between">
+                  <div>
+                    <p className="text-sm">Order Processing</p>
+                    <span className="text-gray-500 text-xs">980 requests</span>
+                  </div>
+                  <span className="font-semibold">980</span>
+                </li>
+                <li className="mb-4 flex items-start justify-between">
+                  <div>
+                    <p className="text-sm">Inventory Management</p>
+                    <span className="text-gray-500 text-xs">860 requests</span>
+                  </div>
+                  <span className="font-semibold">860</span>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className="border border-gray-300 rounded-md p-4">
+            <div>
+              <h4 className="font-semibold text-gray-600 mb-4 flex items-center justify-between">
+                Notifications
+                <GoMail />
+              </h4>
+              <ul>
+                <li className="mb-4 flex items-start gap-3">
+                  <div>
+                    <GoAlert className="text-red-600 inline-block mr-2" />
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Alert: API 'Payment Gateway' is experiencing high error
+                      rates.
+                    </p>
+                    <span className="text-gray-500 text-xs">3 hours ago</span>
+                  </div>
+                </li>
+                <li className="mb-4 flex items-start gap-3">
+                  <div>
+                    <FiInfo className="text-blue-600 inline-block mr-2" />
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Info: New version of API 'User Service' deployed
+                      successfully.
+                    </p>
+                    <span className="text-gray-500 text-xs">6 hours ago</span>
+                  </div>
+                </li>
+                <li className="mb-4 flex items-start gap-3">
+                  <div>
+                    <FiCheckCircle className="text-green-600 inline-block mr-2" />
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Success: Backup completed for 'Order Processing' server.
+                    </p>
+                    <span className="text-gray-500 text-xs">1 day ago</span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </section>
     </section>
   );
 };
