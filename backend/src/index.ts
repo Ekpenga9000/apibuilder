d765e805ef50bf4c5761c74968dbe2da3acf20bb
Louis <ekpenga900@gmail.com>
2025-12-31 19:47:46 -0800

    Solve CORS problem.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,22 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes";
 import userRoutes from "./routes/user.routes";
 import testRoutes from "./routes/test.routes";
+import cors from "cors";
 
-// Routes
+const corsOptions = {
+  origin: process.env.CLIENT_URL, // your frontend server
+  // methods: "GET, HEAD, PUT, PATCH, POST, DELETE", // include DELETE
+  credentials: true,
+  // optionsSuccessStatus: 204,
+};
+
 const app = express();
+app.use(express.json());
 dotenv.config();
+app.use(cors(corsOptions));
 const apiRouter = express.Router();
 app.use("/api", apiRouter);
-app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3001;
 apiRouter.use("/auth", authRoutes);
@@ -17,7 +26,7 @@ apiRouter.use("/users", userRoutes);
 apiRouter.use("/test", testRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Welcome to the API" });
+  res.json({ message: "Welcome to the API", client: process.env.CLIENT_URL });
 });
 
 app.get("/api/health", (req: Request, res: Response) => {
